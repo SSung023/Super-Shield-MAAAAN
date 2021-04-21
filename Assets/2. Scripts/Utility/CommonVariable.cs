115ca340428c7dbd9114478c287c20c27e6baf8f
SSung <50323157+Sung2019@users.noreply.github.com>
2021-04-21 21:12:16 +0900

    4.21. Player animation
    애니메이션 버그 수정
    점프 관련 버그 아직 존재하고 있는데 추후 수정하겠습니다.

--- Assets/2. Scripts/Utility/CommonVariable.cs
+++ Assets/2. Scripts/Utility/CommonVariable.cs
@@ -11,6 +11,7 @@ public class CommonVariable : MonoBehaviour
     
     public enum MoveType
     {
+        // 방패가 없는 상태, 서있는 상태, 달리기, 점프, 대쉬
         EMPTY = 0, IDLE = 1, RUN = 2, JUMP = 3, DASH = 4
     }
 }
