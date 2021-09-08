8ec3c625b5320f5ea74f9ff5d05c163012360192
SSung <50323157+Sung2019@users.noreply.github.com>
2021-09-08 18:28:55 +0900

    09.01 - 2번 작업
    2번. Unity asset 중 하나인 Cinemachine을 이용해서 일정 범위 이상으로 카메라가 나가지 못하게 했습니다.
    이 Cinemachine에 엄청나게 다양한 기능들이 있는 것 같은데, 자료들을 더 찾아봐야 할 것 같습니다.
    
    - Scene내에 Camera bounds 오브젝트가 말 그대로 카메라 범위를 설정했습니다.
    - Cam을 프리팹화했습니다.
    - Prefeb - Util이라는 폴더를 만들어 GameManager, SoundManager, Cam 프리팹을 넣어놨습니다.

--- Assets/2. Scripts/Player/CameraController.cs
+++ Assets/2. Scripts/Player/CameraController.cs
@@ -6,13 +6,26 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    
+    [SerializeField] private Vector2 cameraDistrict_center;
+    [SerializeField] private Vector2 cameraDistrict_size;
+    private float camera_width;
+    private float camera_height;
+    
     private Vector3 minCameraPos;
     private Vector3 maxCameraPos;
     private bool focusOut = false;
 
+
+    private void Start()
+    {
+        camera_height = Camera.main.orthographicSize;
+        camera_width = camera_height * Screen.width / Screen.height;
+    }
     private void FixedUpdate()
     {
         Vector3 targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        //Vector3 targetPos = District_Camera();
         minCameraPos = new Vector3(targetPos.x - 6, targetPos.y - 6, 0);
         maxCameraPos = new Vector3(targetPos.x + 6, targetPos.y + 6, 0);
 
@@ -20,7 +33,7 @@ public class CameraController : MonoBehaviour
         {
             Isfocusout(minCameraPos, maxCameraPos, transform.position);
         }
-
+        
         if(focusOut)
         {
             transform.position = Vector3.Lerp(transform.position, targetPos, Time.fixedDeltaTime * 3f);
@@ -29,40 +42,53 @@ public class CameraController : MonoBehaviour
         {
             transform.position = Vector3.Lerp(transform.position, targetPos, Time.fixedDeltaTime * 0.8f);
         }
-          // print(focusOut);
     }
 
+    private Vector3 District_Camera()
+    {
+        float lx = cameraDistrict_size.x * 0.5f / camera_width;
+        float clampX = Mathf.Clamp(target.transform.position.x, -lx + cameraDistrict_center.x, lx + cameraDistrict_center.x);
+        
+        float ly = cameraDistrict_size.y * 0.5f / camera_height;
+        float clampY = Mathf.Clamp(target.transform.position.y, -ly + cameraDistrict_center.y, ly + cameraDistrict_center.y);
+
+        //transform.position = new Vector3(clampX, clampY, 0);
+        Vector3 targetPos = new Vector3(clampX, clampY, transform.position.z);
+        
+        return targetPos;
+    }
+    
     private void Isfocusout(Vector3 minPos, Vector3 maxPos, Vector3 cameraPos)
     {
         if(cameraPos.x < minPos.x )
         {
             focusOut = true;
-            StartCoroutine(Focusreset());
+            StartCoroutine(ResetFocus());
             return;
         }
         else if (cameraPos.y < minPos.y)
         {
             focusOut = true;
-            StartCoroutine(Focusreset());
+            StartCoroutine(ResetFocus());
             return;
         }
         else if (cameraPos.x > maxPos.x)
         {
             focusOut = true;
-            StartCoroutine(Focusreset());
+            StartCoroutine(ResetFocus());
             return;
 
         }
         else if (cameraPos.y > maxPos.y)
         {
             focusOut = true;
-            StartCoroutine(Focusreset());
+            StartCoroutine(ResetFocus());
             return;
         }
         focusOut = false;
         return;
     }
-    IEnumerator Focusreset()
+    IEnumerator ResetFocus()
     {
         yield return new WaitForSeconds(1.0f);
         focusOut = false;
